59c733890addb62d866f3b52b5e39c91a9f33f41
Ryan Blue <ryanblue468@gmail.com>
2016-10-02 09:59:01 -0400

    Cropped images

--- js/script.js
+++ js/script.js
@@ -24,7 +24,25 @@ function preLoad(){
         "/not-overwatch/img/char/Ana.png",
         "/not-overwatch/img/char/Bastion.png",
         "/not-overwatch/img/char/Dva.png",
-        "/not-overwatch/img/char/Genji.png"
+        "/not-overwatch/img/char/Genji.png",
+        "/not-overwatch/img/char/Hanzo.png",
+        "/not-overwatch/img/char/Junkrat.png",
+        "/not-overwatch/img/char/Lucio.png",
+        "/not-overwatch/img/char/McCree.png",
+        "/not-overwatch/img/char/Mei.png",
+        "/not-overwatch/img/char/Mercy.png",
+        "/not-overwatch/img/char/Pharah.png",
+        "/not-overwatch/img/char/Reaper.png",
+        "/not-overwatch/img/char/Reinhardt.png",
+        "/not-overwatch/img/char/Roadhog.png",
+        "/not-overwatch/img/char/Soldier76.png",
+        "/not-overwatch/img/char/Symmetra.png",
+        "/not-overwatch/img/char/Tobjorn.png",
+        "/not-overwatch/img/char/Tracer.png",
+        "/not-overwatch/img/char/Widowmaker.png",
+        "/not-overwatch/img/char/Winston.png",
+        "/not-overwatch/img/char/Zarya.png",
+        "/not-overwatch/img/char/Zenyetta.png"
     ];
     for(var i = 0; i < images.length; i++ ) 
     {
@@ -42,7 +60,7 @@ function loadScreen(){
 
     ctx.fillStyle = "White"
     ctx.font="50px Georgia";
-    ctx.fillText("loading... " + loadPercent + "%",500,canvas.height/2+20);
+    ctx.fillText("loading... " + loadPercent + "%",10,canvas.height/2+20);
 
 }
 
@@ -69,8 +87,14 @@ function update() {
 }
 
 function draw() {
-    
 
+    drawBackground();
+
+    drawPlatforms();
+
+    drawPlayer();
+
+    drawPlayers();
 }
 
 window.onresize = function(event) {
@@ -81,9 +105,6 @@ function resize() {
     console.log("resizing...");
     canvas.height = 800;
     canvas.width = 1200;
-    
-    ctx.fillStyle = "Black"
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 init();
